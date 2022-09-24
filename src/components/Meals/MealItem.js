0af4fa4ba302f56442bbeeb2ac5608622df17d40
ziyad onji <ziyad.onji@ibm.com>
2022-09-24 18:06:18 +0530

    form

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,5 +1,6 @@
 
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = ({ meal }) => {
     const price = `$${meal.price.toFixed(2)}`;
@@ -8,6 +9,9 @@ const MealItem = ({ meal }) => {
             <h3>{meal.name}</h3>
             <div className={classes.description}>{meal.description}</div>
             <div className={classes.price}>{price}</div>
+        </div>
+        <div>
+            <MealItemForm id={meal.id} />
         </div>
     </div>
 
